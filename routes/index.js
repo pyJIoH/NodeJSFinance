2c9f1f52c9cdb5a5e56014e73a50614d95a20382
pyjioh <pyjioh@gmail.com>
2016-01-27 11:45:47 +0500

    Add simple mogoose usage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected');
+});
+
+var Schema = mongoose.Schema;
+
+var accountingSchema = new Schema({
+  title: String,
+  date: {type: Date, default: Date.now},
+  income: Number,
+  outcome: Number,
+  profit: Number,
+  hidden: {type: Boolean, default: false}
+});
+
+var Accounting = mongoose.model('Accounting', accountingSchema);
+
+var row = new Accounting({
+  title: "OOO APK Rostok",
+  income: 0,
+  outcome: 5000,
+  profit: 300
+});
+
+console.log(row.title);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Express', name: row.title});
 });
 
 module.exports = router;
