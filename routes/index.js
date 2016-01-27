5bc8fdcf4d2fd25f3ac022192d9806ef9168dc6f
asaybel <pyjioh@gmail.com>
2016-01-27 16:34:33 +0500

    Add bootstrap with sample

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,49 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log('Connected');
+    console.log('Connected');
 });
 
 var Schema = mongoose.Schema;
 
 var accountingSchema = new Schema({
-  title: String,
-  date: {type: Date, default: Date.now},
-  income: Number,
-  outcome: Number,
-  profit: Number,
-  hidden: {type: Boolean, default: false}
+    title: String,
+    date: {type: Date, default: Date.now},
+    income: Number,
+    outcome: Number,
+    profit: Number,
+    hidden: {type: Boolean, default: false}
 });
 
 var Accounting = mongoose.model('Accounting', accountingSchema);
 
-var row = new Accounting({
-  title: "OOO APK Rostok",
-  income: 0,
-  outcome: 5000,
-  profit: 300
+var row1 = new Accounting({
+    title: "OOO APK Rostok",
+    income: 0,
+    outcome: 5000,
+    profit: 300
 });
 
-console.log(row.title);
+var row2 = new Accounting({
+    title: "OOO Vozrojdenie",
+    income: 0,
+    outcome: 5000,
+    profit: 300
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express', name: row.title});
+    res.render('index', {
+        title: 'Express',
+        title1: row1.title,
+        income1: row1.income,
+        outcome1: row1.outcome,
+        profit1: row1.profit,
+        title2: row2.title,
+        income2: row2.income,
+        outcome2: row2.outcome,
+        profit2: row2.profit
+    });
 });
 
 module.exports = router;
