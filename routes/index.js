804a9c036476809fcc1068a721dcd335114402b6
asaybel <pyjioh@gmail.com>
2016-01-28 18:39:53 +0500

    Database and client have been divided

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connected');
-});
-
-var Schema = mongoose.Schema;
-
-var accountingSchema = new Schema({
-    title: String,
-    date: {type: Date, default: Date.now},
-    income: Number,
-    outcome: Number,
-    profit: Number,
-    hidden: {type: Boolean, default: false}
-});
-
-var Accounting = mongoose.model('Accounting', accountingSchema);
-
-var row1 = new Accounting({
-    title: "OOO APK Rostok",
-    income: 0,
-    outcome: 5000,
-    profit: 300
-});
-
-var row2 = new Accounting({
-    title: "OOO Vozrojdenie",
-    income: 10000,
-    outcome: 0,
-    profit: 500
-});
+var db = require('database');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Express',
-        title1: row1.title,
-        income1: row1.income,
-        outcome1: row1.outcome,
-        profit1: row1.profit,
-        title2: row2.title,
-        income2: row2.income,
-        outcome2: row2.outcome,
-        profit2: row2.profit
+        title1: db.getData()[0].title,
+        income1: db.getData()[0].income,
+        outcome1: db.getData()[0].outcome,
+        profit1: db.getData()[0].profit,
+        title2: db.getData()[1].title,
+        income2: db.getData()[1].income,
+        outcome2: db.getData()[1].outcome,
+        profit2: db.getData()[1].profit
     });
 });
 
