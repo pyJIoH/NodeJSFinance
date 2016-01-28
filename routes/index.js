dc05d57790e9d5b8d8a688a2f1fcc001ee174512
pyjioh <pyjioh@gmail.com>
2016-01-29 00:05:53 +0500

    Add Jade iteration for table

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var Finance = require('models/finance').Finance;
 
 //var finance = new Finance({
-//    title: "OOO Vozrojdenie",
-//    income: 10000,
+//    title: "Company Name 5 Ltd.",
+//    income: 5000,
 //    outcome: 0,
-//    profit: 500
+//    profit: 200
 //});
 //
 //finance.save(function(err, user, affected) {
@@ -15,19 +15,10 @@ var Finance = require('models/finance').Finance;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var rows = {}
     Finance.find({}, function(err, elems) {
-        rows = elems;
         res.render('index', {
             title: 'Express',
-            title1: rows[0].title,
-            income1:rows[0].income,
-            outcome1: rows[0].outcome,
-            profit1: rows[0].profit,
-            title2: rows[2].title,
-            income2: rows[2].income,
-            outcome2: rows[2].outcome,
-            profit2: rows[2].profit
+            rows: elems
         });
     });
 });
