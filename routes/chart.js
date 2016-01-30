652f17dc8c77905b1566facf80c0d46a8deeb68e
pyjioh <pyjioh@gmail.com>
2016-01-30 18:28:25 +0500

    Add empty chart page

--- routes/chart.js
+++ routes/chart.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET chart page. */
-router.get('/', function(req, res, next) {
-    res.send('Chart here');
+router.get('/', function (req, res, next) {
+    res.render('chart', {
+
+    });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Finance = require('models/finance').Finance;
 
-//var finance = new Finance({
-//    title: "Company Name 5 Ltd.",
-//    income: 5000,
-//    outcome: 0,
-//    profit: 200
-//});
-//
-//finance.save(function(err, user, affected) {
-//    console.log(arguments);
-//});
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Finance.find({}, function(err, rows) {
