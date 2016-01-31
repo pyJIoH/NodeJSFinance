f372476670823798c7d494ce07c707436f418f1f
pyjioh <pyjioh@gmail.com>
2016-01-31 23:31:43 +0500

    Put real data to the chart from DB

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,63 +1,41 @@
 $(document).ready(function () {
-    var data = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
+    $.getJSON('/chart/data', function (data) {
+        if (data) {
+            var titles = [], incomes = [], outcomes = [];
 
-    var options = {
-        ///Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines: true,
-        //String - Colour of the grid lines
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        //Number - Width of the grid lines
-        scaleGridLineWidth: 1,
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines: true,
-        //Boolean - Whether the line is curved between points
-        bezierCurve: true,
-        //Number - Tension of the bezier curve between points
-        bezierCurveTension: 0.4,
-        //Boolean - Whether to show a dot for each point
-        pointDot: true,
-        //Number - Radius of each point dot in pixels
-        pointDotRadius: 4,
-        //Number - Pixel width of point dot stroke
-        pointDotStrokeWidth: 1,
-        //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-        pointHitDetectionRadius: 20,
-        //Boolean - Whether to show a stroke for datasets
-        datasetStroke: true,
-        //Number - Pixel width of dataset stroke
-        datasetStrokeWidth: 2,
-        //Boolean - Whether to fill the dataset with a colour
-        datasetFill: true,
-        //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
-    };
+            data.forEach(function (row) {
+                titles.push(row.title);
+                incomes.push(row.income);
+                outcomes.push(row.outcome);
+            });
 
-    var ctx = chart.getContext("2d");
-    var lineChart = new Chart(ctx).Line(data, options);
+            var chartData = {
+                labels: titles,
+                datasets: [
+                    {
+                        label: "Income",
+                        fillColor: "rgba(201,230,177,0.2)",
+                        strokeColor: "rgba(201,230,177,1)",
+                        pointColor: "rgba(201,230,177,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(201,230,177,1)",
+                        data: incomes
+                    },
+                    {
+                        label: "Outcome",
+                        fillColor: "rgba(225,184,172,0.2)",
+                        strokeColor: "rgba(225,184,172,1)",
+                        pointColor: "rgba(225,184,172,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(225,184,172,1)",
+                        data: outcomes
+                    }
+                ]
+            };
+            var ctx = chart.getContext("2d");
+            var lineChart = new Chart(ctx).Line(chartData, {});
+        }
+    });
 });
--- routes/chart.js
+++ routes/chart.js
@@ -4,11 +4,18 @@ var Finance = require('models/finance').Finance;
 
 /* GET chart page. */
 router.get('/', function (req, res, next) {
-    Finance.find({}, function (err, rows) {
-        res.render('chart', {
-            //'rows': rows || {}
-        });
+    res.render('chart', {
+
     });
 });
+
+/* GET chart data. */
+router.get('/data', function (req, res, next) {
+    Finance.find({}, function (err, rows) {
+        res.json(rows);
+    });
+});
+
+
 
 module.exports = router;
